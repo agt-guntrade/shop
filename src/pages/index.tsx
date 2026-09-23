8922a622f376a447773b39c61cf7d86d16b4f31f
Nico Schett <nico.schett@cronit.io>
2026-09-23 10:11:11 +0200

    feat: remove the website-update disclaimer banner
    Delete the DisclaimerBanner component and its use on the home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,6 @@ import {
 import {SideButtons} from '../components/molecules/buttons/SideButtons'
 import {useContactModal} from '../services/contact'
 import {ScrollSpy} from '../components/molecules/ScrollSpy'
-import DisclaimerBanner from '../components/disclaimer-banner'
 
 interface IndexPageData {
   googleReviews: {
@@ -39,7 +38,6 @@ const IndexPage: React.FC<PageProps<IndexPageData>> = props => {
 
   return (
     <>
-      <DisclaimerBanner />
       <HeroSection
         name="hero"
         latestProducts={props.data.latestProducts.nodes}
